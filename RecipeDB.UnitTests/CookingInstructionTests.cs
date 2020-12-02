0a39f810c4bf030af22b2250191b36cacae9e2ad
Happy user <Happy user@DESKTOP-C8BUOD4>
2020-12-02 17:33:40 -0500

    Added initial code for cooking instructions and ingredients unit tests

---
+++ RecipeDB.UnitTests/CookingInstructionTests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeDB.UnitTests
+{
+    class CookingInstructionTests : TestHelper
+    {
+
+
+        #region field 
+
+        /// <summary>
+        /// field for the repository 
+        /// </summary>
+        private CookingInstructionRepository cookingInstructionRepository;
+
+        #endregion
+
+        #region Setup and Cleanup Methods
+
+        /// <summary>
+        /// Method to perform setup procedures.Runs per test case
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            //setup the connections and context 
+            SetupConnectionAndContext();
+
+            //create the repository for cooking instructions
+            cookingInstructionRepository = new CookingInstructionRepository(Context);
+        }
+
+        /// <summary>
+        /// Method to perform cleanup procedures. Runs per test case
+        /// </summary>
+        [TearDown]
+        public void CleanUp()
+        {
+            //close the connection
+            Connection.Close();
+        }
+
+        #endregion
+
+        #region Add Update Delete Tests
+        #endregion
+
+        #region Other Method Tests
+        #endregion
+
+    }
+}
---
+++ RecipeDB.UnitTests/IngredientRepositoryTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeDB.UnitTests
+{
+    /// <summary>
+    /// Unit test class for testing ingredients repository
+    /// </summary>
+    class IngredientRepositoryTests : TestHelper
+    {
+        #region field 
+
+        /// <summary>
+        /// field for the repository 
+        /// </summary>
+        private IngredientRepository ingredientRepository;
+
+        #endregion
+
+        #region Setup and Cleanup Methods
+
+        /// <summary>
+        /// Method to perform setup procedures.Runs per test case
+        /// </summary>
+        [SetUp]
+        public void Setup()
+        {
+            //setup the connections and context 
+            SetupConnectionAndContext();
+
+            //create the repository for ingredients
+            ingredientRepository = new IngredientRepository(Context);
+        }
+
+        /// <summary>
+        /// Method to perform cleanup procedures. Runs per test case
+        /// </summary>
+        [TearDown]
+        public void CleanUp()
+        {
+            //close the connection
+            Connection.Close();
+        }
+
+        #endregion
+
+        #region Add Update Delete Tests
+        #endregion
+
+        #region Other Method Tests
+        #endregion
+
+    }
+}
