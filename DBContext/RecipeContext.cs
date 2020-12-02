680b971b68ee729264aa26979e1f1775d1a19673
Happy user <Happy user@DESKTOP-C8BUOD4>
2020-12-02 11:32:12 -0500

    Added Db context class

---
+++ DBContext/RecipeContext.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities;
+using System.Dynamic;
+
+namespace DBContext
+{
+    /// <summary>
+    /// Database Context class for a recipe database
+    /// </summary>
+    class RecipeContext : DbContext
+    {
+        #region Entities 
+
+        /// <summary>
+        /// Db set for the recipe entity
+        /// </summary>
+        public virtual DbSet<Recipe> Recipes { get; set; }
+
+        /// <summary>
+        /// Db set for the ingredient entity
+        /// </summary>
+        public virtual DbSet<Ingredient> Ingredients { get; set; }
+
+        /// <summary>
+        /// Db set for the CookingInstruction entity    
+        /// </summary>
+        public virtual DbSet<CookingInstruction> CookingInstructions { get; set; }
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public RecipeContext()
+        {
+
+        }
+
+
+        /// <summary>
+        /// Constructor that takes in a context options
+        /// </summary>
+        /// <param name="options">The options object</param>
+        public RecipeContext(DbContextOptions<RecipeContext> options) : base(options)
+        {
+
+        }
+
+        #endregion 
+    }
+}
